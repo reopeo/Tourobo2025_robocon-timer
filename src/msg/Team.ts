326e0520df15e6674b74972728d475c027f4ee21
reopeo <reopeo0526@gmail.com>
2025-08-31 03:01:18 +0900

    今年に対応

--- src/msg/Team.ts
+++ src/msg/Team.ts
@@ -2,13 +2,17 @@ export interface Team {
   name: string;
   id: string;
   university: string;
-  is_auto: boolean;
 
-  seedlings: number;
-  immigration: boolean;
-  type_1_a: boolean;
-  type_1_b: boolean;
-  type_2: boolean;
+  unlock: number;
+  type_1_a: number;
+  type_1_b: number;
+  type_1_c: number;
+  type_2_a: number;
+  type_2_b: number;
+  type_2_c: number;
+  type_3_a: number;
+  type_3_b: number;
+  type_3_c: number;
   v_goal: boolean;
 
   score: number;
