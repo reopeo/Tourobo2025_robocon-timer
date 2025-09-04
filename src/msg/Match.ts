7d389664395d28665e05c21b646a05d13897fe60
reopeo <reopeo0526@gmail.com>
2025-09-04 15:27:05 +0900

    Add bingo spots.

--- src/msg/Match.ts
+++ src/msg/Match.ts
@@ -17,4 +17,14 @@ export interface Match {
   blue_team: Team;
 
   winner: (typeof Winner)[keyof typeof Winner];
+
+  type_1_a: number;
+  type_1_b: number;
+  type_1_c: number;
+  type_2_a: number;
+  type_2_b: number;
+  type_2_c: number;
+  type_3_a: number;
+  type_3_b: number;
+  type_3_c: number;
 }
